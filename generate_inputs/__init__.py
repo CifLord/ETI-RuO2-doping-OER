a3724b15295a89085c0332b18121d2b472c0603a
Richard Tran <rtran25@juniper.rcdc.uh.edu>
2024-12-13 16:04:15 -0600

    added __init__.py files

---
+++ generate_inputs/__init__.py
@@ -0,0 +1,3 @@
+"""
+Wait! It's not enough to look like Squidward to be opposite, you have to act like him, too. Boy, oh boy, do I like playing the clarinet. I practice and practice all day long, but I never get any better. Now you try. IM SQUIDWARD IM SQUIDWARD IM SQUIDWARD SQUIDWARD SQUIDWARD!
+"""
