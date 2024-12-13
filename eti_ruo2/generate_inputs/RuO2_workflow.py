55e88dd226b986716e9a234f4eff0cc884ef18a1
Richard Tran <rtran25@juniper.rcdc.uh.edu>
2024-12-13 16:29:56 -0600

    added function to load all pre-made structures for workflow

--- eti_ruo2/generate_inputs/RuO2_workflow.py
+++ eti_ruo2/generate_inputs/RuO2_workflow.py
@@ -1,2 +1,30 @@
+import json, glob
+
+from pymatgen.core.surface import *
+
+
+def load_structures():
+    
+    all_ads_dict = {'OOH': [], 'OH': [], 'O': []}
+    for f in glob.glob('../pmg_structures/adsorbates/*'):
+        if '.xyz' not in f:
+            continue
+        n = f.split('/')[-1].replace('.xyz', '')
+        if 'OOH' in n:
+            all_ads_dict['OOH'].append(Molecule.from_file(f))
+        elif 'OH' in n:
+            all_ads_dict['OH'].append(Molecule.from_file(f))
+        else:
+            all_ads_dict['O'].append(Molecule.from_file(f))
+            
+    slabdict = json.load(open('../pmg_structures/RuO2_slabs.json', 'r'))
+    slabdict = {i: Slab.from_dict(slabdict[i]) for i in slabdict.keys()}
+    bulk = Structure.from_dict(json.load(open('../pmg_structures/RuO2_bulk.json', 'r')))
+    
+    return all_ads_dict, slabdict, bulk
+
+
 def doped_RuO2_OER_workflow(dopants_list, nlayers_list):
+
+    all_ads_dict, slabdict, bulk = load_structures()
     return None
