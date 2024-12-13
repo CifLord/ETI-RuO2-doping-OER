1bf1d455c3cd2f521f629ab0b7024858a5f9e410
Richard Tran <rtran25@juniper.rcdc.uh.edu>
2024-12-13 15:08:22 -0600

    added functions to dope slabs and add vacancies

--- generate_inputs/surface_RuO2.py
+++ generate_inputs/surface_RuO2.py
@@ -58,3 +58,49 @@ def get_selective_dynamics(slab, nlayers=3):
     
     return selective_dynamics
 
+def get_adslabs(slab, adscoords, adsconfigs):
+    
+    all_adslabs = []
+    for adscoord in adscoords:
+        for ads in adsconfigs:
+            adslab = slab.copy()
+            adsites = []
+            for a in ads:
+                adslab.append(a.species_string, np.array(adscoord)+a.coords, 
+                              coords_are_cartesian=True, 
+                              properties={'selective_dynamics': [True]*3, 
+                                          'sitetype': 'adsorbate'})
+            all_adslabs.append(adslab)
+        
+    sm = StructureMatcher()
+    if not all_adslabs:
+        return []
+    return [g[0] for g in sm.group_structures(all_adslabs)]
+
+def get_vac_slabs(slab):
+    
+    rounded_fracc = []
+    actual_fracc = []
+    for site in slab:
+        if site.species_string == 'O':
+            coord = round(site.coords[2], 0)
+            if coord not in rounded_fracc:
+                rounded_fracc.append(coord)
+                actual_fracc.append(site.coords[2])
+                
+    actual_fracc = sorted(actual_fracc)
+    vac_slabs = []
+    for i, site in enumerate(slab):
+        if site.species_string == 'O':
+            if actual_fracc[-1]-0.25 < site.coords[2] < actual_fracc[-1]+0.25:
+                vac_slab = slab.copy()
+                vacsite = slab[i]
+                vac_slab.remove_sites([i])
+                setattr(vac_slab, 'vacsite', vacsite) 
+                vac_slabs.append(vac_slab)
+
+    sm = StructureMatcher()
+    if not vac_slabs:
+        return []
+    return [g[0] for g in sm.group_structures(vac_slabs)]
+
